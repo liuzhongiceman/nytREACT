69027789cc72c41c639d06454d8a95ea3ac68ca7
liuzhongiceman <zhongliu884@gmail.com>
2018-03-29 00:04:24 -0700

    change

---
+++ app/components/common/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <hr />
+    <p className="pull-right">
+      
+      Made by UCI BOOTCAMP  with lots and lots of <i className="fa fa-heart" aria-hidden="true" />
+    </p>
+  </footer>
+);
+
+export default Footer;
